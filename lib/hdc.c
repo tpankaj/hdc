65f5ae878fb92899cb06374dec0b79b1679c5361
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2016-11-01 21:23:20 -0700

    Implement dot_product function
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- lib/hdc.c
+++ lib/hdc.c
@@ -9,6 +9,24 @@
  * computeSumHV -> compute_sum_hv
  * lookupItemMemory -> lookup_item_memory
  * dotProduct -> dot_product
+ * entrywiseProduct -> entrywise_product
  * circShift -> circ_shift
  * norm -> norm
 */
+
+/**
+ * Calculates dot product of OP1 and OP2.
+ * @param op1  First operand
+ * @param op2  Second operand
+ * @param len  Length of vectors
+ * @return Dot product of OP1 and OP2
+ */
+static double dot_product(double op1[], double op2[], size_t len)
+{
+    double accum = 0.0;
+    for (int i = 0; i < len; i++)
+    {
+        accum += op1[i] * op2[i];
+    }
+    return accum;
+}
