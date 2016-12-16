9fd457b3d6935462efda036dd6ac7bc5b1872178
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2016-12-15 18:26:18 -0800

    Make working test framework
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- test/test_hdc.c
+++ test/test_hdc.c
@@ -1,5 +1,22 @@
 #include "unity.h"
 
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+void test_hdc()
+{
+    int a = 2;
+    TEST_ASSERT_EQUAL(2, a);
+}
+
 int main(int argc, char* argv[])
 {
+    UNITY_BEGIN();
+    RUN_TEST(test_hdc);
+    return UNITY_END();
 }
